6d1ec393e280c1bb164e5b3b24196d17d68f9881
AsriFox <asrifox@yandex.ru>
2024-01-28 19:29:48 +0300

    Added swaylock config file

--- hypr/scripts/lock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-swaylock \
---clock \
---screenshots \
---effect-blur 4x2 \
---fade-in 3 \
-"$@"
--- swaylock/idle.sh
+++ swaylock/idle.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 swayidle \
-timeout 300 '~/.config/hypr/scripts/lock.sh --grace 5' \
+timeout 300 'swaylock --grace 5' \
 timeout 600 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' \
 timeout 1800 'systemctl suspend' \
-before-sleep '~/.config/hypr/scripts/lock.sh'
+before-sleep 'swaylock'
