76613a532e094ea39fbd38504bacd049aa717700
AsriFox <asrifox@yandex.ru>
2024-02-28 21:06:54 +0300

    Replaced swaylock/swayidle with hyprlock/hyrpidle

--- swaylock/idle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-swayidle \
-timeout 300 'swaylock --grace 5' \
-timeout 600 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' \
-timeout 1800 'systemctl suspend' \
-before-sleep 'swaylock'
